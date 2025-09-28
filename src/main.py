ce0ca53343e1d741f9d1f318a8991f05988e37b3
Daniel S Jeremiah <danielshunom2@gmail.com>
2025-09-27 21:43:54 -0400

    temporary code in main showing how to use the current wrapper classes

--- src/main.py
+++ src/main.py
@@ -1,1 +1,48 @@
+import data_processing.data_loader as dl
+import data_processing.data_split as ds
 
+def main():
+    loader = dl.DataLoader()
+    loader.createDataDir()
+    loader.loadDataFromDataset()
+    loader.preprocessDataset()
+    loader.filterDataSet()
+
+    dataset = loader.getFilteredDataset()
+    window = dl.createWindowFromEvents(dataset)
+
+    dl.addColumns(
+        dataset,
+        window,
+        "target",
+        "rt_from_stimulus",
+        "rt_from_trialstart",
+        "stimulus_onset",
+        "response_onset",
+        "correct",
+        "response_type"
+    )
+
+    metadata = window.get_metadata()
+    dl.inspectMetadata(metadata)
+
+    splitter = ds.DataSplitter(metadata, window)
+    splitter.setUniqueData("subject")
+    splitter.filterSubjects()
+    splitter.performSanityCheck()
+    splitter.splitToTrainTest()
+
+    train_set = splitter.getTrainSet()
+    valid_set = splitter.getValidSet()
+    test_Set  = splitter.getTestSet()
+
+    print("Number of examples in each split minirelease")
+
+    print(
+        f"Train Set     : \t{len(train_set)}\n"
+        f"Validation Set: \t{len(valid_set)}\n"
+        f"Test set      : \t{len(test_Set)}\n"
+    )
+
+if __name__ == "__main__":
+    main();
